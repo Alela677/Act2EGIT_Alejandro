8e4e804d00456ad44971cab3ce307dbd153de302
Alejandro <alejandrolealastorga@gmail.com>
2021-11-30 12:27:50 +0100

    Array 20 numeros

---
+++ EGIT/src/PRUEB/Array_egit.java
@@ -0,0 +1,20 @@
+package PRUEB;
+
+public class Array_egit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] Array = new int[20];
+
+		for (int i = 0; i < Array.length; i++) {
+			Array[i] = (int) (Math.random() * 10);
+			System.out.print(Array[i] + " ");
+		}
+
+		System.out.println();
+		
+
+	}
+
+}
