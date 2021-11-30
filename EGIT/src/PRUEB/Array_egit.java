c7a4867239c37e60db5ef1a4689c2e02fe9787d7
Alejandro <alejandrolealastorga@gmail.com>
2021-11-30 12:33:50 +0100

    Commit rama 2

--- EGIT/src/PRUEB/Array_egit.java
+++ EGIT/src/PRUEB/Array_egit.java
@@ -6,12 +6,15 @@ public class Array_egit {
 		// TODO Auto-generated method stub
 
 		int[] Array = new int[20];
-
+		int contar = 0;
 		for (int i = 0; i < Array.length; i++) {
 			Array[i] = (int) (Math.random() * 10);
+			contar++;
 			System.out.print("[");
 			System.out.print(Array[i] + " ");
 			System.out.print("]");
+			System.out.println();
+			System.out.print(contar);
 		}
 
 		System.out.println();
